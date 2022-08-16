d23a17c97f80df69e778e385159750a460a18275
Hammad <hammadfaiz172@gmail.com>
2022-08-16 10:29:22 -0400

    commit tree f9ed41d7cd46b362f65b31c4bcdcb224d81d564c

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,38 @@
-# your code goes here
+require "pry"
+class Person 
+
+  attr_accessor :name, :bank_account, :happiness, :hygiene
+
+  def initialize(name, bank_account=25, happiness=8, hygiene=8 )
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def bank_account(amount)
+    @bank_account = amount
+  end
+
+  def happiness (value)
+    if value >= 0 
+      @happiness = value
+    # elsif value <= 10
+    #   @happiness = value
+    end
+  end
+
+  def hygiene(value)
+    if value >= 0
+      @hygiene = value
+    end
+  end
+
+end
+
+faiz = Person.new("Faiz")
+faiz.bank_account(90)
+faiz.happiness(80)
+faiz.hygiene(88)
+
+binding.pry
